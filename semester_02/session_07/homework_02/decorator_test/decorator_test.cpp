21c3f86d32c4da449da1ec92a0ae2e3b0a7f4da6
Дмитрий Анатольевич Костюченко <dima.kostyuchenko@gmail.com>
2025-06-01 02:16:17 +0300

    Edited the code

--- semester_02/session_07/homework_02/decorator_test/decorator_test.cpp
+++ semester_02/session_07/homework_02/decorator_test/decorator_test.cpp
@@ -7,11 +7,14 @@
 int main()
 {
     using namespace std;
+ 
+    /*
     auto text_block = 
         make_unique<ItalicText>(make_unique<BoldText>(make_unique<Text>()));
     text_block->render("Hello world");
     std::cout << "\n";
 
+    
     auto paragraph = make_unique<Paragraph>(make_unique<Text>());
     paragraph->render("Hello world");
     std::cout << "\n";
@@ -19,9 +22,12 @@ int main()
     auto reversed = make_unique<Reversed>(make_unique<Text>());
     reversed->render("Hello world");
     std::cout << "\n";
-
+     */
     auto link = make_unique<Link>(make_unique<Text>());
     link->render("netology.ru", "Hello world");
-        
+   
+    
+    std::cout << '\n';
+    std::system("pause");
     return 0;
 }
