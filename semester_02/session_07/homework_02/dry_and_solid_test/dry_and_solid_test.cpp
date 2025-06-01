76feb48ecffa7f207ac5ba3cfa2db3730f3f1d33
Дмитрий Анатольевич Костюченко <dima.kostyuchenko@gmail.com>
2025-06-01 13:16:04 +0300

    Edited the code

--- semester_02/session_07/homework_02/dry_and_solid_test/dry_and_solid_test.cpp
+++ semester_02/session_07/homework_02/dry_and_solid_test/dry_and_solid_test.cpp
@@ -1,42 +1,50 @@
 ﻿// dry_and_solid_test.cpp
 #include "include/basic_data.h"
+#include "include/tags.h"
 #include <cstdlib>
 #include <fstream>
-#include <functional>
 #include <iostream>
 #include <string>
 
-std::string operator""_s(const char* str, std::size_t) { return std::string{ str }; }
+namespace my_literals
+{
+	static std::string operator""_s(const char* str, std::size_t) 
+	{ 
+		return std::string{ str }; 
+	}
+
+	template<typename T, typename... Args>
+	void send_to(std::ofstream& of, T tag, Args&&... args)
+	{
+		const solid::basic_data raw{ std::forward<Args>(args)... };
+		raw.send_to(of, tag);
+	}
+}
 
 int main()
 {
-	using namespace std;
-
-	if (ofstream log("test.txt"); log.is_open())
+	if (std::ofstream log("test.txt"); log.is_open())
 	{
 		using namespace solid;
+		using namespace my_literals;
 
-		basic_data text{ 'A','.',' ',
+		const basic_data text{ 'A','.',' ',
 			"Strings are objects that represent sequences of characters"_s, '.' };
+		text.send_to(log, text_tag{});
 
-		basic_data html{ "<meta "_s, "property = \"og:image:alt\" "_s,
-			"content = \"Image description\""_s,	">"_s };
+		send_to(log, html_tag{},
+			"<meta "_s,
+			"property = \"og:image:alt\" "_s, "content = \"Image description\""_s,
+			'>');
 
-		basic_data json{ "\"street\": \"Moskovskoe sh., 10, fl. 101\", "_s,
-			"\"city\": \"Leningrad\", "_s, "\"postalCode\": "_s, 101101 };
-
-		void(decltype(text):: * send_to_text)(std::ostream&, text_tag) const = &decltype(text)::send_to;
-		std::invoke(send_to_text, &text, log, text_tag{});
-
-		void(decltype(html):: * send_to_html)(std::ostream&, html_tag) const = &decltype(html)::send_to;
-		std::invoke(send_to_html, &html, log, html_tag{});
-
-		void(decltype(json):: * send_to_json)(std::ostream&, json_tag) const = &decltype(json)::send_to;
-		std::invoke(send_to_json, &json, log, json_tag{});
+		send_to(log, json_tag{},
+			"\"street\": \"Moskovskoe sh., 10, fl. 101\", "_s,
+			"\"city\": \"Leningrad\", "_s,
+			"\"postalCode\": "_s, 101101);
 	}
-	else { cout << "Could not open the file"; }
+	else { std::cout << "Could not open the file"; }
 
-	cout << "\n\n";
-	system("pause");
+	std::cout << "\n\n";
+	std::system("pause");
 	return 0;
 }
--- semester_02/session_07/homework_02/dry_and_solid_test/include/basic_data.h
+++ semester_02/session_07/homework_02/dry_and_solid_test/include/basic_data.h
@@ -3,55 +3,30 @@
 #ifndef BASIC_DATA_H_IN_MY_PROJECT
 #define BASIC_DATA_H_IN_MY_PROJECT
 
-#include <ostream>
+#include <fstream>
 #include <tuple>
 #include <utility>
 
 namespace solid
 {
-	struct text_tag
-	{
-		decltype(auto) operator()() const
-		{
-			return std::make_pair("\nbeginning of formatted text\n",
-				"\nend of formatted text\n");
-		}
-	}; /// struct text_tag
-
-	struct html_tag
-	{
-		decltype(auto) operator()() const
-		{
-			return std::make_pair("\n<html>\t<head>\n\t\t", "\n\t</head>\n</html>\n");
-		}
-	}; /// struct html_tag
-
-	struct json_tag
-	{
-		decltype(auto) operator()() const
-		{
-			return std::make_pair("\n\"address\" : {", "}\n");
-		}
-	}; /// struct json_tag
-
 	template<typename... Args>
 	class basic_data
 	{
 	public:
 		explicit basic_data(Args&& ...args) : _args{ std::make_tuple(std::forward<Args>(args) ...) } {}
-		template<typename Tag>
-		void send_to(std::ostream& os, Tag func) const
+		template<typename T>
+		void send_to(std::ofstream& of, T tag) const
 		{
-			const auto&& [beg, end] = func();
-			os << beg;
-			send_to(os, std::index_sequence_for<Args...>{});
-			os << end;
+			const auto&& [beg, end] = tag();
+			of << beg;
+			send_to(of, std::index_sequence_for<Args...>{});
+			of << end;
 		}
 	private:
 		template<std::size_t... I>
-		void send_to(std::ostream& os, std::index_sequence<I...>) const
+		void send_to(std::ofstream& of, std::index_sequence<I...>) const
 		{
-			((os << std::get<I>(_args)), ...);
+			((of << std::get<I>(_args)), ...);
 		}
 		std::tuple<Args...> _args{};
 	}; /// class basic_data
