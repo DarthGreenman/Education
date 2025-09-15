8e53ee753f55e847ee07daa9cfd4d3dacd598acc
DarthGreenman <dima.kostyuchenko@gmail.com>
2025-09-15 19:41:22 +0300

    Added a solution to the first task on learning Unreal Engine

---
+++ semester_02/session_10/FirstProject/Source/FirstProject/Private/SuperBall.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SuperBall.h"
+
+// Sets default values
+ASuperBall::ASuperBall()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	Super::PrimaryActorTick.bCanEverTick = true;
+	Mesh = UObject::CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	Super::SetRootComponent(Mesh);
+}
+
+// Called when the game starts or when spawned
+void ASuperBall::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASuperBall::ShowActorInformation() const
+{
+	UE_LOG(LogTemp, Display, TEXT("Координаты актора x:%f, y:%f, z:%f"), Super::GetActorLocation().X, Super::GetActorLocation().Y,
+		Super::GetActorLocation().Y);
+}
+
+FVector ASuperBall::MoveLawMotion(const FVector& Location, const FMovementParameters& Parameters) const
+{
+	const auto Phase = Parameters.Frequency * Super::GetWorld()->GetTimeSeconds();
+	return FVector{Location.X, Location.Y + Phase, Location.Z + Parameters.Amplitude * sin(Phase)};
+}
+
+// Called every frame
+void ASuperBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ semester_02/session_10/FirstProject/Source/FirstProject/Public/SuperBall.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SuperBall.generated.h"
+
+class UStaticMeshComponent;
+
+USTRUCT(BlueprintType)
+struct FMovementParameters
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Amplitude{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Frequency{};
+};
+
+UCLASS()
+class FIRSTPROJECT_API ASuperBall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASuperBall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void ShowActorInformation() const;
+
+	UFUNCTION(BlueprintCallable)
+	FVector MoveLawMotion(const FVector& Location, const FMovementParameters& Parameters) const;
+
+protected:
+	UPROPERTY(VisibleAnywhere);
+	UStaticMeshComponent* Mesh;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
