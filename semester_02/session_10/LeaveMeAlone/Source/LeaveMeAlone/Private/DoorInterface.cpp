47a3936151762d435e5a21c830c1ed84ffabd713
DarthGreenman <dima.kostyuchenko@gmail.com>
2025-12-28 15:04:33 +0300

    Edited the code for the Leave Me Alone project

--- semester_02/session_10/LeaveMeAlone/Source/LeaveMeAlone/Private/DoorInterface.cpp
+++
@@ -1,6 +0,0 @@
-// LeaveMeAlone Game by Netologiya. All Rights Reserved.
-
-
-#include "DoorInterface.h"
-
-// Add default functionality here for any IDoorInterface functions that are not pure virtual.
